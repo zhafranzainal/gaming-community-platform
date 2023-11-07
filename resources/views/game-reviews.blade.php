323de0fe39916ea0b71fe819444ea1e806eca101
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-08 04:13:14 +0800

    Add review

--- resources/views/game-reviews.blade.php
+++ resources/views/game-reviews.blade.php
@@ -3,24 +3,39 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
+
             <h1>Game Reviews</h1>
+
             <p>
-                Explore in-depth game reviews contributed by our community of gamers. Discover what others think about
-                the latest releases, classics, and hidden gems. If you're passionate about gaming, this is the place to
-                dive into detailed reviews and share your own thoughts.
+                Explore in-depth game reviews contributed by our community of passionate gamers. Discover what others think
+                about the latest releases, classics, and hidden gems. This is the place to dive into detailed reviews and
+                share your own thoughts.
             </p>
+
             <h2>Latest Game Reviews</h2>
-            <ul>
-                <li>
-                    <a href="#">Game Title 1</a> - Reviewed by User123
+
+            <ul class="list-group">
+
+                <li class="list-group-item">
+                    <div class="media">
+
+                        <img src="https://img.freepik.com/premium-vector/man-character_665280-46967.jpg"
+                            class="mr-3 rounded-circle" style="max-width: 5%" alt="User123's Avatar">
+
+                        <div class="media-body">
+                            <h5 class="mt-0"><a href="#">Life is Strange: True Colors</a></h5>
+                            <p>Reviewed by <a href="#">jeffchang076</a></p>
+                            <p>
+                                A captivating and immersive gaming experience that will keep you on the edge of your seat.
+                            </p>
+                            <a href="#" class="btn btn-primary">Read Review</a>
+                        </div>
+
+                    </div>
                 </li>
-                <li>
-                    <a href="#">Game Title 2</a> - Reviewed by Gamer456
-                </li>
-                <li>
-                    <a href="#">Game Title 3</a> - Reviewed by Player789
-                </li>
+
             </ul>
+
         </div>
     </div>
 @endsection
