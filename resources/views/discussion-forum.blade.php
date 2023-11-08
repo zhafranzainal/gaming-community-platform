f5dcb6bc96c1019f931032ebac4d808566adce9b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-08 17:30:09 +0800

    Add discussion

--- resources/views/discussion-forum.blade.php
+++ resources/views/discussion-forum.blade.php
@@ -7,24 +7,25 @@
             <h1>Join the Discussion</h1>
 
             <p>
-                Welcome to the gaming community's discussion forum. Engage with fellow gamers, share your insights,
-                and discuss everything related to your favorite games. Whether you're looking for strategies, game
+                Welcome to the gaming community's discussion forum. Engage with fellow gamers, share your insights, and
+                discuss everything related to your favorite games. Whether you're looking for strategies, game
                 recommendations, or just a place to chat, you're in the right spot.
             </p>
 
             <h2>Popular Discussions</h2>
 
-            <ul>
-                <li>
-                    <a href="#">Discussion Topic 1</a> - Started by User123
-                </li>
-                <li>
-                    <a href="#">Discussion Topic 2</a> - Started by Gamer456
-                </li>
-                <li>
-                    <a href="#">Discussion Topic 3</a> - Started by Player789
-                </li>
-            </ul>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><a href="#">Is this game very difficult?</a></h5>
+                    <p class="card-text">Started by <a href="#">jeffchang076</a></p>
+                    <p class="card-text">
+                        Join the discussion about the best in-game strategies for taking down the final boss in Mortal
+                        Kombat 11. jeffchang076's insights are valuable, and other gamers are sharing their experiences as
+                        well.
+                    </p>
+                    <a href="#" class="btn btn-primary">Join Discussion</a>
+                </div>
+            </div>
 
         </div>
     </div>
